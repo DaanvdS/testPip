9ff61ea246c2f199a1d5580ef7f5981526bd89b2
Spek,D.S. van der (Daan) <d.s.vanderspek@students.uu.nl>
2022-04-07 16:29:17 +0200

    Test Pip package v0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='testPip',
-    version='0.0.1',
+    version='0.0.3',
     author='Daan',
     author_email='d.s.vanderspek@students.uu.nl',
     description='Schema package for spider',
