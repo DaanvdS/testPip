1bb65bed89c07a1b64a5804cd8883b4d1ba664d1
Spek,D.S. van der (Daan) <d.s.vanderspek@students.uu.nl>
2022-04-07 16:16:36 +0200

    Test Pip package v0.0.2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name='testPip',
-    version='1.0.1',
+    version='0.0.2',
     author='Daan',
     author_email='d.s.vanderspek@students.uu.nl',
     description='Schema package for spider',
     long_description='Schema package for spider',
     long_description_content_type="text/markdown",
     url='https://github.com/DaanvdS/testPip',
-    packages="testPip",
+    packages=["testPip"],
     license='MIT'
 )
